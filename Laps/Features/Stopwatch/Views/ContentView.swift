76df35b067ab83ec5b4f6af78af38cd66b5913db
Oscar Karlsson <karlsson.oscar@gmail.com>
2020-02-09 15:07:42 +0100

    Stacks equally distributed

--- Laps/Features/Stopwatch/Views/ContentView.swift
+++ Laps/Features/Stopwatch/Views/ContentView.swift
@@ -5,27 +5,31 @@ struct ContentView: View {
     @ObservedObject var stopWatchViewModel = StopwatchViewModel()
 
     var body: some View {
-        VStack(alignment: .center, spacing: 10) {
-
+        GeometryReader { geometry in
             VStack(alignment: .center, spacing: 10) {
-                Text(self.stopWatchViewModel.timeLabelText)
+                VStack(alignment: .center, spacing: 10) {
+                    Text(self.stopWatchViewModel.timeLabelText)
 
-                Button(action: {
-                    self.stopWatchViewModel.startButtonAction()
-                }) {
-                    Text(self.stopWatchViewModel.startButtonText)
+                    Button(action: {
+                        self.stopWatchViewModel.startButtonAction()
+                    }) {
+                        Text(self.stopWatchViewModel.startButtonText)
+                    }
+
+                    Button(action: {
+                        self.stopWatchViewModel.lapButtonAction()
+                    }) {
+                        Text(self.stopWatchViewModel.lapButtonText)
+                    }
                 }
+                .frame(width: geometry.size.width, height: geometry.size.height / 2)
 
-                Button(action: {
-                    self.stopWatchViewModel.lapButtonAction()
-                }) {
-                    Text(self.stopWatchViewModel.lapButtonText)
-                }
-            }
+                Divider()
 
-            VStack(alignment: .center, spacing: 10) {
-                List(self.stopWatchViewModel.laps) { lap in
-                    Text(lap.time.presentable)
+                VStack(alignment: .center, spacing: 10) {
+                    List(self.stopWatchViewModel.laps) { lap in
+                        Text(lap.time.presentable)
+                    }
                 }
             }
         }
